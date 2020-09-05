8f022997d5376a339c7b43abf02100164bea2bf1
Jakob Hellermann <jakob.hellermann@protonmail.com>
2020-09-05 13:34:45 +0200

    refactor: simplify attribute parsing

--- bevy-contrib-inspector-derive/src/attrs.rs
+++ bevy-contrib-inspector-derive/src/attrs.rs
@@ -1,18 +1,22 @@
 fn parse_inspectable_attributes(
     input: syn::parse::ParseStream,
-) -> syn::Result<impl Iterator<Item = (Box<syn::Expr>, Box<syn::Expr>)>> {
-    use syn::parse::Parse;
-    let x: syn::punctuated::Punctuated<_, syn::Token![,]> =
-        input.parse_terminated(syn::ExprAssign::parse)?;
+) -> syn::Result<impl Iterator<Item = (syn::Ident, syn::Expr)>> {
+    let parse_attribute = |input: syn::parse::ParseStream| {
+        let ident: syn::Ident = input.parse()?;
+        let _eq_token: syn::Token![=] = input.parse()?;
+        let expr: syn::Expr = input.parse()?;
+        Ok((ident, expr))
+    };
 
-    Ok(x.into_iter()
-        .map(|expr_assign| (expr_assign.left, expr_assign.right)))
+    input
+        .parse_terminated::<_, syn::Token![,]>(parse_attribute)
+        .map(IntoIterator::into_iter)
 }
 
 /// extracts [(min, 8), (field, vec2(1.0, 1.0))] from `#[inspectable(min = 8, field = vec2(1.0, 1.0))]`,
 pub fn inspectable_attributes(
     attrs: &[syn::Attribute],
-) -> impl Iterator<Item = (Box<syn::Expr>, Box<syn::Expr>)> + '_ {
+) -> impl Iterator<Item = (syn::Ident, syn::Expr)> + '_ {
     attrs
         .iter()
         .filter(|attr| attr.path.get_ident().map_or(false, |p| p == "inspectable"))
